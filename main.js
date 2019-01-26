5e07dcddb1fccc3cd986d1ab5e6d59a609cc8da3
Drake Lambert <drake-lambert@outlook.com>
2019-01-26 10:58:39 -0600

    cleaning main

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-const planets = [];
-const satellites = [];
-const shuttles = [];
-const players = [];
-
 function setup() {
 	document.body.style.margin = 0;
 	createCanvas(windowWidth, windowHeight - 5);
 
 	// Create planets
-	[...Array(10).keys()].map(i => new Planet(Math.random() * width, Math.random() * height)).forEach(planet => planets.push(planet));
+	let planets = [...Array(10).keys()].map(i => new Planet(Math.random() * width, Math.random() * height));
 
 	// Create player
 	mainPlayer = new Player('cornflowerblue');
 	planets[0].owner = mainPlayer;
-	players.push(mainPlayer);
 
 	// Create satellites
-	satellites.push(new Satellite(mainPlayer, planets[0]));
+	new Satellite(mainPlayer, planets[0]);
 }
 
 const drawT = new Trigger();
