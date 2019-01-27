c8b4bab02f1d84cc4f8af4ce773dc7e7afcba9be
Drake Lambert <drake-lambert@outlook.com>
2019-01-26 19:30:39 -0600

    ai

--- js/ai.js
+++ js/ai.js
@@ -1,14 +1,30 @@
 class AI {
 	constructor() {
 		this.player = new Player('salmon');
+		this.attackSpeed = 5000;
+		window.setTimeout(this.attackLoop.bind(this), 2000);
+	}
+
+	attackLoop() {
+		this.attackNearestPlanet();
+		window.setTimeout(this.attackLoop.bind(this), this.attackSpeed);
 	}
 
 	attackNearestPlanet() {
+		let pair = this.getNearestPlanets();
 
+		if (pair) {
+			this.player.planets.forEach(planet => {
+				if (Math.random() > .5) {
+					this.player.selectPlanet([planet]);
+					this.player.selectPlanet([pair.opposingPlanet]);
+				}
+			});
+		}
 	}
 
 	/**
-	 * @returns {Planet}
+	 * @returns {Pair}
 	 */
 	getNearestPlanets() {
 		let minimumDistance = Number.POSITIVE_INFINITY;
@@ -17,12 +33,22 @@ class AI {
 		let otherPlanets = planets.filter(planet => !this.player.planets.includes(planet));
 		this.player.planets.forEach(aiPlanet => {
 			otherPlanets.forEach(otherPlanet => {
-				let disance = dist(aiPlanet.x, aiPlanet.y, otherPlanet.x, otherPlanet.y);
-				if (disance < minimumDistance) {
-					minimumDistance = disance;
-					closestPair = {}
+				let distance = dist(aiPlanet.x, aiPlanet.y, otherPlanet.x, otherPlanet.y);
+				if (distance < minimumDistance) {
+					minimumDistance = distance;
+					closestPair = new Pair(aiPlanet, otherPlanet);
 				}
 			});
 		});
+
+		return closestPair;
+	}
+
+}
+
+class Pair {
+	constructor(teamPlanet, opposingPlanet) {
+		this.teamPlanet = teamPlanet;
+		this.opposingPlanet = opposingPlanet;
 	}
 }
