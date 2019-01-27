1db3b944cdb932287792e2d8092f1400399f636f
Remy <remirezmcc@gmai.com>
2019-01-26 22:59:14 -0600

    fixed

--- js/explosion.js
+++ js/explosion.js
@@ -10,7 +10,7 @@ class Explosion {
     draw() {
         push();
         imageMode(CENTER);
-        image(explosion,this.shuttle.x,this.shuttle.y,15,15,(this.frameX * 66), (this.frameY * 66), 66, 66);
+        image(explosion,this.shuttle.x,this.shuttle.y,15,15,(this.frameX * 64), (this.frameY * 66), 60, 66);
         if (++this.frameX % 8 === 0 && this.frameY < 3) {
             this.frameX = 0;
             this.frameY++;
